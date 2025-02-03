8c15233b502787e42707ad87ef19fb70e27453bc
LemonDangNguyen <nguyendangdonguyen@gmail.com>
2025-02-03 14:57:02 +0700

    haha

--- app/src/main/java/com/example/collageimage/SplashActivity.kt
+++ app/src/main/java/com/example/collageimage/SplashActivity.kt
@@ -4,24 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Handler
 import android.os.Looper
-import android.view.ViewGroup
 import com.example.collageimage.base.BaseActivity
 import com.example.collageimage.databinding.ActivitySplashBinding
 import com.example.collageimage.language.LanguageActivity
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
-import com.nlbn.ads.banner.BannerPlugin
-import com.nlbn.ads.callback.AdCallback
-import com.nlbn.ads.util.Admob
-import com.nlbn.ads.util.ConsentHelper
-import com.nmh.base.project.extensions.invisible
-import com.nmh.base.project.extensions.visible
-
 import com.nmh.base.project.helpers.CURRENT_LANGUAGE
 import com.nmh.base.project.helpers.IS_SHOW_BACK
 import com.nmh.base.project.model.LanguageModel
 import com.nmh.base.project.sharepref.DataLocalManager
-import com.nmh.base.project.utils.AdsConfig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -30,15 +19,11 @@ import kotlinx.coroutines.withContext
 import java.util.Locale
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
-import kotlin.text.Typography.dagger
 
 @SuppressLint("CustomSplashScreen")
-
 class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
 
     override fun isHideNavigation(): Boolean = true
-
-    private var interCallback: AdCallback? = null
 
     override fun setUp() {
         if (DataLocalManager.getLanguage(CURRENT_LANGUAGE) == null) {
@@ -51,68 +36,25 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding
         if (haveNetworkConnection()) {
             CoroutineScope(Dispatchers.IO).launch {
                 val remote = async { loadRemoteConfig() }
-                if (remote.await())
+                if (remote.await()) {
                     withContext(Dispatchers.Main) {
-                        interCallback = object : AdCallback() {
-                            override fun onNextAction() {
-                                super.onNextAction()
-                                startActivity()
-                            }
-                        }
-
-                        val consentHelper = ConsentHelper.getInstance(this@SplashActivity)
-                        if (!consentHelper.canLoadAndShowAds()) consentHelper.reset()
-
-                        consentHelper.obtainConsentAndShow(this@SplashActivity) {
-                            loadBanner()
-                            //load trước native language
-                            AdsConfig.loadNativeLanguage(this@SplashActivity)
-                            AdsConfig.loadNativeLanguageSelect(this@SplashActivity)
-
-                            Admob.getInstance().loadSplashInterAds2(this@SplashActivity, getString(R.string.inter_splash), 0, interCallback)
-                        }
+                        startActivity()
                     }
+                }
             }
-        } else Handler(Looper.getMainLooper()).postDelayed({ startActivity() }, 1500)
+        } else {
+            Handler(Looper.getMainLooper()).postDelayed({ startActivity() }, 1500)
+        }
     }
 
     private suspend fun loadRemoteConfig(): Boolean {
         return suspendCoroutine { continuation ->
-
-            val configSetting = FirebaseRemoteConfigSettings.Builder()
-                .setMinimumFetchIntervalInSeconds(BuildConfig.Minimum_Fetch)
-                .build()
-
-            val remoteConfig = FirebaseRemoteConfig.getInstance().apply {
-                setConfigSettingsAsync(configSetting)
-            }
-
-            remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults) /*file này lấy trên firebase*/
-
-            remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    //chỗ này sẽ get value key remote (làm và lấy theo trên link excel trên nhóm a Huy gửi, nhân bản ra sheet riêng)
-                }
-                continuation.resume(true)
-            }
+            continuation.resume(true)
         }
     }
 
     private fun startActivity() {
         DataLocalManager.setBoolean(IS_SHOW_BACK, false)
         startIntent(Intent(this, LanguageActivity::class.java), true)
-    }
-
-    private fun loadBanner() {
-        if (haveNetworkConnection() && AdsConfig.isLoadFullAds() && AdsConfig.isLoadFullAds()) {
-            binding.rlBanner.visible()
-            val config = BannerPlugin.Config()
-            val cbFetchInterval = AdsConfig.cbFetchInterval
-            config.defaultRefreshRateSec = cbFetchInterval
-            config.defaultCBFetchIntervalSec = cbFetchInterval
-            config.defaultAdUnitId = getString(R.string.banner_all)
-            config.defaultBannerType = BannerPlugin.BannerType.Adaptive
-            Admob.getInstance().loadBannerPlugin(this, binding.banner, binding.shimmer as ViewGroup, config)
-        } else binding.rlBanner.invisible()
     }
 }
